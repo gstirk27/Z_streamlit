22e43e45986cd24b12fb08de539adeba6189eea0
Gracie <gstirk@byu.edu>
2024-12-04 20:52:26 -0700

    each tab has a pie chart now

--- main.py
+++ main.py
@@ -26,12 +26,14 @@ df.drop(df[df['race'] == ' '].index, inplace=True)
 
 st.title("Census of Hyrule")
 
+with st.sidebar:
+    numgames = st.number_input("Only include games that have _____ many NPCs listed",5,280)
+    numraces = st.number_input("Only include races that have more than ____ entries",5,300)
+
 tab1, tab2, tab3 = st.tabs(['Main','Game','Race'])
 
 with tab1:
     st.subheader("Table of Counts")
-    numgames = st.number_input("Only include games that have _____ many NPCs listed",5,280)
-    numraces = st.number_input("Only include races that have more than ____ entries",5,300)
     newdf = shrink_table(df,numgames,numraces)
     table = table_of_counts(newdf['race'],newdf['title'])
     st.dataframe(table)
@@ -53,18 +55,27 @@ with tab1:
     st.pyplot(fig2)
 
 with tab2:
-    nice = shrink_table(df,5,5)
+    nice = shrink_table(df,numgames,numraces)
     games = st.multiselect("Pick games to look at:", nice['title'].unique())
     gamestable = only_these_games(nice,games)
     table2 = table_of_counts(gamestable['race'],gamestable['title'])
     st.dataframe(table2)
 
+    st.subheader("Characters in the Legend of Zelda games")
+    #st.write(f"These are the proportions of NPCs in each game with more than {numgames} NPCs and {numraces} in each race.")
+    fig2, ax2 = plt.subplots()
+    labels = gamestable['title'].value_counts().index.tolist()
+    ax2.pie(gamestable['title'].value_counts(),pctdistance=1.2,autopct='%1.1f%%',)
+    ax2.legend(labels,fontsize=4,loc='upper right')
+    st.pyplot(fig2)
+
 
 with tab3:
-    nicer = shrink_table(df,5,5)
+    nicer = shrink_table(df,numgames,numraces)
     race = st.multiselect("Pick some fantasy races to look at:", nicer['race'].unique())
     racetable = only_these_races(nicer,race)
     table3 = table_of_counts(racetable['title'],racetable['race'])
+    #st.write("Double-click on the game title to see the full name")
     st.dataframe(table3)
 
     st.subheader("Demographics (Races) of Hyrule")
